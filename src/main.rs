08ea14bb989983e856ed5edd75524a21a71ecc81
valkyrie_pilot <valk@randomairborne.dev>
2023-10-30 22:22:22 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+mod util;
+
+use std::sync::Arc;
+
+use crate::util::{validate_discord_sig, wait_for_shutdown};
+use axum::response::{IntoResponse, Response};
+use axum::{body::Bytes, extract::State, http::HeaderMap, Json, Router};
+use ed25519_dalek::{Signature, Verifier, VerifyingKey};
+use reqwest::Client as HttpClient;
+use tokio::signal::unix::{signal, SignalKind};
+use twilight_http::{client::InteractionClient, Client as DiscordClient};
+use twilight_model::{
+    application::interaction::{Interaction, InteractionType},
+    channel::message::MessageFlags,
+    http::interaction::{InteractionResponse, InteractionResponseType},
+    id::{marker::ApplicationMarker, Id},
+};
+use twilight_util::builder::embed::EmbedBuilder;
+use twilight_util::builder::InteractionResponseDataBuilder;
+
+#[tokio::main]
+async fn main() {
+    let pub_key_string = std::env::var("DISCORD_PUBKEY").expect("Expected DISCORD_PUBKEY");
+    let token = std::env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN");
+    let pub_key_bytes: [u8; 32] = hex::decode(pub_key_string)
+        .expect("Discord pubkey was invalid hex")
+        .try_into()
+        .expect("Discord pubkey was wrong length (32 bytes, 64 chars expected)");
+    let pubkey = VerifyingKey::from_bytes(&pub_key_bytes).expect("Invalid Ed25519 verifying key");
+
+    let discord = twilight_http::Client::new(token);
+    let my_id = discord
+        .current_user_application()
+        .await
+        .unwrap()
+        .model()
+        .await
+        .unwrap()
+        .id;
+    let http = reqwest::Client::builder()
+        .user_agent("smbwdiscord/rolelinker (valk@randomairborne.dev)")
+        .build()
+        .unwrap();
+
+    let state = AppState {
+        discord: Arc::new(discord),
+        http,
+        pubkey: Arc::new(pubkey),
+        my_id,
+    };
+    let app = Router::new()
+        .route("/interaction-callback", axum::routing::any(handle))
+        .with_state(state);
+    axum::Server::bind(&([0, 0, 0, 0], 8080).into())
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(wait_for_shutdown())
+        .await
+        .unwrap()
+}
+
+pub async fn handle(
+    State(state): State<AppState>,
+    headers: HeaderMap,
+    body: Bytes,
+) -> Result<Json<InteractionResponse>, Error> {
+    validate_discord_sig(&state, &headers, body.as_ref())?;
+    let interaction: Interaction = serde_json::from_slice(&body)?;
+    let resp = match interaction.kind {
+        InteractionType::ApplicationCommand => state.submit(interaction).await,
+        _ => InteractionResponse {
+            kind: InteractionResponseType::Pong,
+            data: None,
+        },
+    };
+    Ok(Json(resp))
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    discord: Arc<DiscordClient>,
+    http: HttpClient,
+    pubkey: Arc<VerifyingKey>,
+    my_id: Id<ApplicationMarker>,
+}
+
+impl AppState {
+    pub async fn submit(&self, interaction: Interaction) -> InteractionResponse {
+        let this = self.clone();
+        tokio::spawn(async move { this.process_wrap(interaction).await });
+        InteractionResponse {
+            kind: InteractionResponseType::DeferredChannelMessageWithSource,
+            data: Some(
+                InteractionResponseDataBuilder::new()
+                    .flags(MessageFlags::EPHEMERAL)
+                    .build(),
+            ),
+        }
+    }
+
+    async fn process_wrap(&self, interaction: Interaction) {
+        let interaction_token = interaction.token.clone();
+        if let Err(source) = self.process(interaction).await {
+            if let Err(effect) = self.report_err(&interaction_token, &source).await {
+                eprintln!(
+                    "Encountered error processing command: {source:?} \
+                     with error trying to report: {effect:?}"
+                );
+            } else {
+                eprintln!("Encountered error processing command: {source:?}");
+            }
+        }
+    }
+
+    async fn report_err(&self, token: &str, error: &Error) -> Result<(), Error> {
+        let client = self.interaction();
+        let err_embed = EmbedBuilder::new().description(error.to_string()).build();
+        client.create_followup(token).embeds(&[err_embed])?.await?;
+        Ok(())
+    }
+
+    fn interaction(&self) -> InteractionClient<'_> {
+        self.discord.interaction(self.my_id)
+    }
+
+    async fn process(&self, interaction: Interaction) -> Result<(), Error> {
+        // todo: this is where to pick up tomorrow
+        Ok(())
+    }
+
+    async fn eligible(&self, src: &str, discord: &str) -> Result<bool, Error> {
+        let body = self
+            .http
+            .get(format!("https://www.speedrun.com/users/{src}"))
+            .send()
+            .await?
+            .text()
+            .await?;
+        // todo: validate that this used also has verified run
+        Ok(body.contains(discord))
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("ed25519-dalek signature error")]
+    Dalek(#[from] ed25519_dalek::SignatureError),
+    #[error("hex decode error")]
+    Hex(#[from] hex::FromHexError),
+    #[error("serde-json error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("twilight-http error: {0}")]
+    TwilightHttp(#[from] twilight_http::Error),
+    #[error("twilight-validate embed error: {0}")]
+    TwilightValidateEmbed(#[from] twilight_validate::embed::EmbedValidationError),
+    #[error("twilight-validate message error: {0}")]
+    TwilightValidateMessage(#[from] twilight_validate::message::MessageValidationError),
+    #[error("Missing X-Signature-Ed25519 header")]
+    MissingSignatureHeader,
+    #[error("Missing X-Signature-Timestamp header")]
+    MissingTimestampHeader,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        self.to_string().into_response()
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,39 @@
+use crate::{AppState, Error};
+use axum::http::HeaderMap;
+use ed25519_dalek::{Signature, Verifier};
+use tokio::signal::unix::{signal, SignalKind};
+
+pub async fn wait_for_shutdown() {
+    let mut int = signal(SignalKind::interrupt()).unwrap();
+    let mut quit = signal(SignalKind::quit()).unwrap();
+    let mut term = signal(SignalKind::terminate()).unwrap();
+    tokio::select! {
+        _ = int.recv() => {},
+        _ = quit.recv() => {},
+        _ = term.recv() => {}
+    }
+}
+
+pub fn validate_discord_sig(
+    state: &AppState,
+    headers: &HeaderMap,
+    body: &[u8],
+) -> Result<(), Error> {
+    let sig_arr = hex::decode(
+        headers
+            .get("X-Signature-Ed25519")
+            .ok_or(Error::MissingSignatureHeader)?,
+    )?;
+    let sig = Signature::from_slice(&sig_arr)?;
+    let timestamp = headers
+        .get("X-Signature-Timestamp")
+        .ok_or(Error::MissingTimestampHeader)?;
+    let to_be_verified: Vec<u8> = timestamp
+        .as_bytes()
+        .iter()
+        .chain(body.iter())
+        .copied()
+        .collect();
+    state.pubkey.verify(to_be_verified.as_slice(), &sig)?;
+    Ok(())
+}
